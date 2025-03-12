981024c353e131dc65c2fbd0533e8ac1408c1c2d
EurisOchoa <104750294+EurisOchoa@users.noreply.github.com>
2025-03-11 21:23:35 -0400

    Sneaked-away 5 gun
    Esta parte se mete el arma en el juego y se configura para dispararle a los enemigos

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from settings import *
 from player import Player
 from sprites import *
+from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -9,20 +11,55 @@ class Game:
         # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Survivor')
+        pygame.display.set_caption('Sneaked-away')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
 
         # groups 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
 
-        # sprites
-        self.player = Player((400,300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60,100), randint(50,100)
-            CollisionSprite((x,y), (w,h), (self.all_sprites, self.collision_sprites))
+        self.setup()
+
+        # gun timer
+        self.can_shoot = True
+        self.shoot_time = 0 
+        self.gun_cooldown = 100
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('images', 'gun', 'bullet.png')).convert_alpha()
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
+        
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+        
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -35,12 +72,15 @@ class Game:
                     self.running = False
 
             # update 
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
+            print(self.bullet_sprites)
 
         pygame.quit()
 
